52e92b0f8ad3abb03779dc214f29350010070ada
Алексей Кольцов <koltsov2022@yandex.ru>
2023-09-11 22:27:46 +0300

    First stable version

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,16 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
+from rest_framework.authtoken import views
+
+
+from .views import PostViewSet
+
+
+router = SimpleRouter()
+router.register('posts', PostViewSet, basename='posts')  # получаем список всех постов или создаём новый пост
+
 
 urlpatterns = [
+    path('v1/', include(router.urls)),
+    path('v1/api-token-auth/', views.obtain_auth_token),
 ]
